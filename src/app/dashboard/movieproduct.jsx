3f271c12016d92fc8b1b61c9673362e8f8ce54b6
godspower <godspjona@gmail.com>
2025-03-22 18:16:43 +0100

    work in process

--- src/app/dashboard/movieproduct.jsx
+++ src/app/dashboard/movieproduct.jsx
@@ -17,14 +17,14 @@ const MovieProduct = () => {
 
     return (
         <div className="w-screen">
-            <div className="w-full overflow-x-auto mb-4">
+            <div className="w-full overflow-x-auto ">
                 <div className="flex gap-4 w-max">
                     {movieProduct.map((product, index) => (
                         <div key={index} className="flex-none flex flex-col items-center gap-2 rounded-lg">
                             <img
                                 src={product.image}
                                 alt={`Movie ${index}`}
-                                className="w-[150px] h-[200px] object-cover rounded-lg"
+                                className="md:w-[150px] md:h-[200px] w-[100px] h-[150px] object-cover rounded-lg"
                             />
                             <p className="text-white">{product.title}</p>
                         </div>
@@ -38,7 +38,21 @@ const MovieProduct = () => {
                             <img
                                 src={product.image}
                                 alt={`Movie ${index + movieProduct.length}`}
-                                className="w-[150px] h-[200px] object-cover rounded-lg"
+                                className="md:w-[150px] md:h-[200px] w-[100px] h-[150px] object-cover rounded-lg"
+                            />
+                            <p className="text-white">{product.title}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="w-full overflow-x-auto">
+                <div className="flex gap-4 w-max">
+                    {movieProduct.map((product, index) => (
+                        <div key={index + movieProduct.length} className="flex-none flex flex-col items-center gap-2 rounded-lg">
+                            <img
+                                src={product.image}
+                                alt={`Movie ${index + movieProduct.length}`}
+                                className="md:w-[150px] md:h-[200px] w-[100px] h-[150px] object-cover rounded-lg"
                             />
                             <p className="text-white">{product.title}</p>
                         </div>
