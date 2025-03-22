a6d25bc5fa8fb013b51ec6717fba225098c04794
godspower <godspjona@gmail.com>
2025-03-22 18:20:24 +0100

    work in process

--- src/app/dashboard/movieproduct.jsx
+++ src/app/dashboard/movieproduct.jsx
@@ -16,8 +16,9 @@ const MovieProduct = () => {
     ];
 
     return (
-        <div className="w-screen">
-            <div className="w-full overflow-x-auto ">
+        <div className="w-screen space-y-5">
+            <p className='text-base text-white pl-3'>Trending Videos</p>
+            <div className="w-full overflow-x-auto scrollbar-hide ">
                 <div className="flex gap-4 w-max">
                     {movieProduct.map((product, index) => (
                         <div key={index} className="flex-none flex flex-col items-center gap-2 rounded-lg">
@@ -31,7 +32,8 @@ const MovieProduct = () => {
                     ))}
                 </div>
             </div>
-            <div className="w-full overflow-x-auto">
+            <p className='text-base text-white pl-3'>Recommeded movies</p>
+            <div className="w-full overflow-x-auto scrollbar-hide">
                 <div className="flex gap-4 w-max">
                     {movieProduct.map((product, index) => (
                         <div key={index + movieProduct.length} className="flex-none flex flex-col items-center gap-2 rounded-lg">
@@ -45,7 +47,8 @@ const MovieProduct = () => {
                     ))}
                 </div>
             </div>
-            <div className="w-full overflow-x-auto">
+            <p className='text-base text-white pl-3'>Suggestions</p>
+            <div className="w-full overflow-x-auto scrollbar-hide">
                 <div className="flex gap-4 w-max">
                     {movieProduct.map((product, index) => (
                         <div key={index + movieProduct.length} className="flex-none flex flex-col items-center gap-2 rounded-lg">
