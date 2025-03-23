d0045cc6706631b5c574b77ede8ad4319ef3cea8
godspower <godspjona@gmail.com>
2025-03-24 00:51:55 +0100

    work in process

--- src/app/dashboard/movieproduct.jsx
+++ src/app/dashboard/movieproduct.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import { IoChevronForwardSharp } from "react-icons/io5";
 
 const MovieProduct = () => {
@@ -15,6 +15,16 @@ const MovieProduct = () => {
         { image: 'imh.jpg', title: 'Movie 6' },
         { image: 'imh.jpg', title: 'Movie 4' },
     ];
+    
+    const [movieList, setMovieList] = useState([]);
+    const getMovieProduct = () => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=c0ebac5ff2fd75ab2eb0202a38c61031')
+        .then(response => response.json())
+        .then(data => setMovieList(data.results))
+    }
+    useEffect(() => {
+        getMovieProduct();
+    }, []);
 
     return (
         <div className=" md:space-y-13 space-y-10 ">
@@ -25,14 +35,14 @@ const MovieProduct = () => {
                 </div>
                 <div className='overflow-x-auto scrollbar-hide '>
                     <div className="flex gap-4 w-max">
-                        {movieProduct.map((product, index) => (
+                        {movieList.map((movie, index) => (
                             <div key={index} className="flex-none flex flex-col items-center gap-2 rounded-lg">
                                 <img
-                                    src={product.image}
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}` }
                                     alt={`Movie ${index}`}
                                     className="md:w-[150px] md:h-[200px] w-[100px] h-[150px] object-cover rounded-lg"
                                 />
-                                <p className="text-white">{product.title}</p>
+                                <p className="text-white">{movie.title.slice(0,15)}</p>
                             </div>
                         ))}
                     </div>
