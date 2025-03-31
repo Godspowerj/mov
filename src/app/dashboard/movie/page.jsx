e8ba36afaf6eeb9edf717e10d2bf63e5ff2c251c
godspower <godspjona@gmail.com>
2025-03-31 16:41:16 +0100

    work in process

--- src/app/dashboard/movie/page.jsx
+++ src/app/dashboard/movie/page.jsx
@@ -24,7 +24,7 @@ const Movies= () => {
       return (
         <div className="min-h-screen">
           <div className="">
-            <div className="grid lg:grid-cols-8 grid-cols-3 gap-1 w-full md:w-auto">
+            <div className="grid lg:grid-cols-8 grid-cols-3 gap-2 py-3 w-full md:w-auto">
               {movieList.map((movie, index) => (
                 <div
                   key={index}
@@ -33,10 +33,10 @@ const Movies= () => {
                   <img
                     src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                     alt={`Movie ${index}`}
-                    className="w-[100px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] object-cover rounded-lg"
+                    className="w-[150px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] object-cover rounded-lg"
                   />
                   <p className="text-white text-sm md:text-base 2xl:text-5xl">
-                    {movie.title.length > 15 ? `${movie.title.slice(0, 15)}...` : movie.title}
+                    {movie.title.length > 15 ? `${movie.title.slice(0, 10)}...` : movie.title}
                   </p>
                 </div>
               ))}
