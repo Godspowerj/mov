6f35c97fa1226599aec50786e1439143389a5c64
godspower <godspjona@gmail.com>
2025-04-03 23:35:53 +0100

    work in process

---
+++ src/app/component/skeletonCard.jsx
@@ -0,0 +1,8 @@
+const SkeletonCard = () => {
+    return (
+        <div className="flex-none w-[200px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] bg-gray-800 animate-pulse rounded-lg"></div>
+    );
+  };
+  
+  export default SkeletonCard;
+  
--- src/app/dashboard/movieproduct.jsx
+++ src/app/dashboard/movieproduct.jsx
@@ -1,24 +1,26 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { IoChevronForwardSharp } from "react-icons/io5";
+import SkeletonCard from '../component/skeletonCard';
 
 const MovieProduct = () => {
     const [movieList, setMovieList] = useState([]);
     const [tvList, setTvList] = useState([]);
     const [upComingList, setUpComingList] = useState([]);
     const [topRatedList, setTopRatedList] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     const fetchAllData = async () => {
         try {
             const res = await fetch('/api/movies'); 
             const data = await res.json();
             console.log(data);
-            
 
             setMovieList(data.popularMovies);
             setTopRatedList(data.topRatedMovies); 
             setTvList(data.popularTVShows);
             setUpComingList(data.upcomingMovies); 
+            setLoading(false); 
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -30,16 +32,16 @@ const MovieProduct = () => {
 
     return (
         <div className="space-y-10 md:space-y-13 2xl:space-y-20">
-            <Section title="Trending ❤️‍🔥" items={movieList} />
-            <Section title="Top Rated ❤️‍🔥" items={topRatedList} />
-            <Section title="Popular TV Shows" items={tvList} />
-            <Section title="Upcoming Movies" items={upComingList} />
+            <Section title="Trending ❤️‍🔥" items={movieList} loading={loading} />
+            <Section title="Top Rated ❤️‍🔥" items={topRatedList} loading={loading} />
+            <Section title="Popular TV Shows" items={tvList} loading={loading} />
+            <Section title="Upcoming Movies" items={upComingList} loading={loading} />
         </div>
     );
 };
 
 // Section Component
-const Section = ({ title, items }) => (
+const Section = ({ title, items, loading }) => (
     <div className="w-full space-y-6 xl:space-y-8 2xl:space-y-10">
         <div className="flex justify-between items-center px-4">
             <p className="text-base md:text-[24px] 2xl:text-5xl text-white font-medium">{title}</p>
@@ -47,27 +49,30 @@ const Section = ({ title, items }) => (
                 more <IoChevronForwardSharp />
             </p>
         </div>
+        
         <div className="overflow-x-auto scrollbar-hide">
-            <div className="flex gap-2 w-full md:w-auto ">
-                {items.map((item, index) => (
-                    <div key={index} className="flex-none flex flex-col bg-gray-900 items-start gap-1 rounded-lg">
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                            alt={`Item ${index}`}
-                            className="w-[200px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] object-cover rounded-lg"
-                        />
-                        <div className='flex flex-col gap-1 px-2 py-1'>
-                           <span className='text-gray-300 text-[12px] '>
-                            {item.release_date || item.first_air_date}
-                            <p>⭐ {item.vote_average.toFixed(1)}</p>
-                        </span>
-                        <p className="text-white text-sm md:text-base 2xl:text-5xl">
-                            {(item.title || item.name)?.slice(0, 17)}
-                        </p> 
+            <div className="flex gap-2 w-full md:w-auto">
+                {loading
+                    ? [...Array(20)].map((_, i) => <SkeletonCard key={i} />)
+                    : items.map((item, index) => (
+                        <div key={index} className="flex-none flex flex-col bg-gray-900 items-start gap-1 rounded-lg">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                                alt={`Item ${index}`}
+                                className="w-[200px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] object-cover rounded-lg"
+                            />
+                            <div className='flex flex-col gap-1 px-2 py-1'>
+                                <span className='text-gray-300 text-[12px]'>
+                                    {item.release_date || item.first_air_date}
+                                    <p>⭐ {item.vote_average.toFixed(1)}</p>
+                                </span>
+                                <p className="text-white text-sm md:text-base 2xl:text-5xl">
+                                    {(item.title || item.name)?.slice(0, 17)}
+                                </p> 
+                            </div>
                         </div>
-                        
-                    </div>
-                ))}
+                    ))
+                }
             </div>
         </div>
     </div>
