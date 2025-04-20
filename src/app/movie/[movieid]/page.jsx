c67b2e82c22db7d5967057ca0f7ee94f4ffb2a61
godspower <godspjona@gmail.com>
2025-04-20 01:56:38 +0100

    work in process

--- src/app/movie/[movieid]/page.jsx
+++ src/app/movie/[movieid]/page.jsx
@@ -1,14 +1,18 @@
 
 // import Footer from '@/app/component/footer';
 // import Movies from '@/app/dashboard/movies/page';
+import Footer from '@/app/component/footer';
+import Sidebar from '@/app/component/sidebar';
 import Image from 'next/image';
 import React from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoChevronForwardSharp } from "react-icons/io5";
+import { RiMovie2Line } from "react-icons/ri";
 
 
 const Moviepage = async ({ params }) => {
 
-  const movieid = await params.movieid
+  const { movieid } = params;
   console.log(movieid);
 
   try {
@@ -22,46 +26,79 @@ const Moviepage = async ({ params }) => {
     console.log(data)
 
     return (
-      <div className='bg-color h-screen'>
-        <div className='flex bg-color  items-center justify-between fixed left-0 right-0 top-0 z-50 shadow-2xl  px-4 '>
-          <div className="flex items-center space-x-2 text-white text-2xl md:flex font-semibold">
-          <RxHamburgerMenu className='text-white text-3xl' />
-            <Image src='/lolo.png' width={70} height={70} alt='logo' />
-            <span>MovStream</span>
-          </div>
-        </div>
-
-        <div className=' px-4 md:pt-10 mx-auto '>
-          <div className="relative mx-auto">
-            <img
-              src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
-              alt={`${data.title}`}
-              className="w-[800px] h-auto "
-            />
-            <div className="absolute inset-0 flex items-center justify-center">
-              <button className="bg-gray-200 bg-opacity-0 rounded-full p-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="true"
-                  viewBox="0 0 24 24"
-                  width="40"
-                  height="40"
-                  clas
-                >
-                  <path d="M8 5v14l11-7z" />
-                </svg>
-              </button>
+      <>
+        <div className='bg-color min-h-screen'>
+          <div className='flex bg-color  items-center justify-between fixed left-0 right-0 top-0 z-50 shadow-2xl  px-4 '>
+            <div className="flex items-center space-x-2 text-white text-2xl md:flex font-semibold">
+              <RxHamburgerMenu className='text-white text-3xl' />
+              <Image src='/lolo.png' width={70} height={70} alt='logo' />
+              <span>MovStream</span>
             </div>
           </div>
-          <div className='py-10'>
-            <h1 className="text-2xl font-bold">{data.title}</h1>
-            <p>{data.release_data}</p>
-            <p className="text-gray-700">{data.overview}</p>
+
+          <div className=' md:px-20 md:pt-20 mx-auto'>
+            <div className="relative mx-auto  md:flex block justify-center items-center h-full">
+              <div>
+                <div className='text-white'>
+                  <h1 className=''>Movie Details</h1>
+                </div>
+
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${data.backdrop_path}`}
+                  alt={`${data.title}`}
+                  className="w-[800px] h-auto "
+                />
+                <div className="absolute inset-0 flex items-center justify-center">
+                  <button className="bg-gray-200 bg-opacity-0 rounded-full p-4">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="true"
+                      viewBox="0 0 24 24"
+                      width="40"
+                      height="40"
+
+                    >
+                      <path d="M8 5v14l11-7z" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div className='flex-start justify-center items-center text-white md:w-[25rem] md:h-[25rem] md:bg-black bg-opacity-50'>
+                <div className='flex items-center text-center '>
+                  <h1 className="text-2xl font-bold">{data.title}</h1>
+                  <IoChevronForwardSharp className='text-gray-400' />
+                </div>
+                {data.genres.map((genre) => (
+                  <span key={genre.id} className="text-gray-300 text-[12px] space-x-2">
+                    {genre.name},
+                  </span>
+                ))}
+                <span className='flex items-center text-gray-300 text-[14px] '>
+                  <RiMovie2Line />|
+                  {data.release_date || data.first_air_date} |
+                  <p>⭐ {data.vote_average.toFixed(1)}</p>
+                </span>
+
+
+                <h1>Resources</h1>
+              </div>
+
+
+
+            </div>
+            <div className='py-10'>
+
+              <p>{data.release_data}</p>
+              <p className="text-gray-700">{data.overview}</p>
+            </div>
           </div>
-          
+
+
+
         </div>
-        
-      </div>
+
+      </>
     )
 
   } catch (error) {
