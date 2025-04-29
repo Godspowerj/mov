8aec26b9b84e302b189a79c3afc8ffe29d6f74ac
godspower <godspjona@gmail.com>
2025-04-29 19:30:12 +0100

    work in process

--- src/app/component/skeletonCard.jsx
+++ src/app/component/skeletonCard.jsx
@@ -1,6 +1,6 @@
 const SkeletonCard = () => {
     return (
-        <div className="flex-none w-[200px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] bg-gray-800 animate-pulse rounded-lg"></div>
+        <div className="flex-none w-[100px] h-[150px] lg:w-[150px] lg:h-[200px] 2xl:w-[600px] 2xl:h-[800px] bg-gray-800 animate-pulse rounded-lg"></div>
     );
   };
   
